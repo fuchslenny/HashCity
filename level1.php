c88d2ae95bd1be5f60ab7b11e9773466441eb235
BenizzelPvZ <ben.daesler@gmail.com>
2025-12-03 21:13:02 +0100

    level1 Familiencontainer padding hinzugefügt, dass die Items besser reinpassen

--- level1.php
+++ level1.php
@@ -433,6 +433,7 @@
         }
         .family-list-container {
             max-height: 250px;
+            padding: 0 5px;
             overflow-y: auto;
         }
         .list-group-item.to-do-family {
